b4c1e3a000248e2481026fd519e60db34dfb365c
dinidamy <dinidamayanti3029@gmail.com>
2021-01-18 17:36:03 +0700

    Forth Commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 class Hello{ 
     public static void main(String[] args){ 
-        System.out.println("Dini: Aplikasi Java 2021"); 
+        System.out.println("Judul"); 
+        System.out.println("Dini: Aplikasi Java 2020"); 
+        System.out.println("--- *** ---"); 
     } 
 }
